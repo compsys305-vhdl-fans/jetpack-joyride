6513541732e7a8a8136fae842a908910768df63f
Parmjot Singh <parmjotsinghrobot@gmail.com>
2026-05-18 11:45:10 +1200

    feat: instantiate mouse and LFSR

--- src/top_jetpack_joyride.vhd
+++ src/top_jetpack_joyride.vhd
@@ -17,6 +17,9 @@ ENTITY top_jetpack_joyride IS
         hex4 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
         hex5 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
 
+        ps2_clk : INOUT STD_LOGIC;
+        ps2_dat : INOUT STD_LOGIC;
+
         vga_r : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
         vga_g : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
         vga_b : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -25,8 +28,64 @@ ENTITY top_jetpack_joyride IS
     );
 END ENTITY top_jetpack_joyride;
 
-ARCHITECTURE port_map OF top_jetpack_joyride IS
+ARCHITECTURE rtl OF top_jetpack_joyride IS
     -- here we would declare components
+    COMPONENT mouse IS
+        PORT (
+            clock_25Mhz, reset	 		: IN STD_LOGIC;
+            mouse_data					: INOUT STD_LOGIC;
+            mouse_clk 					: INOUT STD_LOGIC;
+            left_button, right_button	: OUT STD_LOGIC;
+            out_mouse_x 				: OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+            out_mouse_y 				: OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+        );
+    END COMPONENT mouse;
+
+    COMPONENT lfsr IS
+        PORT (
+            clock : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            enable : IN STD_LOGIC;
+            mouse_x : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+            mouse_y : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+            load : IN STD_LOGIC;
+            random_out : OUT STD_LOGIC_VECTOR(19 DOWNTO 0)
+        );
+    END COMPONENT lfsr;
+
+    SIGNAL clock_25 : STD_LOGIC := '0';
+
+    -- mouse signals
+    SIGNAL left_button, right_button : STD_LOGIC;
+    SIGNAL mouse_x, mouse_y : STD_LOGIC_VECTOR(9 DOWNTO 0);
+    SIGNAL mouse_reset : STD_LOGIC := '0';
+
+    -- lfsr signals
+    SIGNAL lfsr_reset : STD_LOGIC := '0';
+    SIGNAL random_num : STD_LOGIC_VECTOR(19 DOWNTO 0);
+    
 BEGIN
     -- placeholder; we should have port maps and stuff, but ideally no logic here (apart from logic inversion for active-low buttons and stuff)
-END ARCHITECTURE port_map;
+    mouse_inst: mouse port map(
+        clock_25Mhz => clock_25,
+        reset => mouse_reset,
+        mouse_data => ps2_dat,
+        mouse_clk => ps2_clk,
+        left_button => left_button,
+        right_button => right_button,
+        out_mouse_x => mouse_x,
+        out_mouse_y => mouse_y
+    );
+
+    lfsr_inst: lfsr port map(
+        clock => clock_50,
+        reset => lfsr_reset, -- currently unused
+        enable => '1',  -- always enabled
+        mouse_x => mouse_x,
+        mouse_y => mouse_y,
+        load => left_button,  -- re-seed on left click
+        random_out => random_num
+    );
+
+    mouse_reset <= NOT KEY(0);  -- active low reset
+END ARCHITECTURE rtl;
