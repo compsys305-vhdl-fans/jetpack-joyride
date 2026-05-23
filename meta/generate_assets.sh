33114df33d958c33ee707c256c2491603eff2345
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-05-23 13:58:47 +1200

    fix: multi animation palettes generated differing palettes

--- meta/generate_assets.sh
+++ meta/generate_assets.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-set -e
+set -ex
 
 for dir in ../res/*; do
 	if [[ -d "$dir" ]]; then
@@ -7,7 +7,7 @@ for dir in ../res/*; do
 		if [[ -e "${pngs[1]}" ]]; then
 			uv run image_conv.py --palette-only --vhdl-output "$dir/palette.vhd" "${pngs[@]}"
 			for img in "${pngs[@]}"; do
-				uv run image_conv.py --no-vhdl "$img"
+				uv run image_conv.py --no-vhdl "$img" --palette-images "${pngs[@]}"
 			done
 		fi
 	fi
